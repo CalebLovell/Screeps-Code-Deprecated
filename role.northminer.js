68cfb35b9231f998f4a4cdea86105fd2891c5319
Caleb Lovell <caleblovell1@gmail.com>
2018-08-01 00:54:41 -0400

    fix miners with else so they go to their rooms

--- role.northminer.js
+++ role.northminer.js
@@ -3,52 +3,54 @@ module.exports = {
     if (creep.room.name != 'E54S48') {
       var northRoom = new RoomPosition(29, 19, 'E54S48');
       creep.moveTo(northRoom);
-    } let sources = creep.room.find(FIND_SOURCES);
-    var chosenSource;
-    var containerBySource;
-    for (var s in sources) {
-      var nearByMiners = sources[s].pos.findInRange(FIND_MY_CREEPS, 1, {
-        filter: (c) => c.memory.role == 'miner' && c != creep
-      });
-      //console.log("soureces: " + sources[s]);
-      containerBySource = sources[s].pos.findInRange(FIND_STRUCTURES, 1, {
+    } else {
+      let sources = creep.room.find(FIND_SOURCES);
+      var chosenSource;
+      var containerBySource;
+      for (var s in sources) {
+        var nearByMiners = sources[s].pos.findInRange(FIND_MY_CREEPS, 1, {
+          filter: (c) => c.memory.role == 'miner' && c != creep
+        });
+        //console.log("soureces: " + sources[s]);
+        containerBySource = sources[s].pos.findInRange(FIND_STRUCTURES, 1, {
+          filter: {
+            structureType: STRUCTURE_CONTAINER
+          }
+        })
+        if (containerBySource.length == 0) {
+          continue;
+        }
+        if (nearByMiners.length == 0) {
+          chosenSource = s;
+          break;
+        }
+      }
+      //console.log("chosensource: " + chosenSource);
+      let allContainers = creep.room.find(FIND_STRUCTURES, {
         filter: {
           structureType: STRUCTURE_CONTAINER
         }
-      })
-      if (containerBySource.length == 0) {
-        continue;
-      }
-      if (nearByMiners.length == 0) {
-        chosenSource = s;
-        break;
-      }
-    }
-    //console.log("chosensource: " + chosenSource);
-    let allContainers = creep.room.find(FIND_STRUCTURES, {
-      filter: {
-        structureType: STRUCTURE_CONTAINER
-      }
-    });
-    //console.log(allContainers)
-    var onContainer = false;
-    for (var i in allContainers) {
-      if (creep.pos.isEqualTo(allContainers[i])) {
-        onContainer = true;
-      }
-    }
-    //console.log("cont:" + container);
-    if (onContainer) {
-      //console.log("on container");
-      var sourceToHarvest = creep.pos.findInRange(FIND_SOURCES, 1);
-      creep.harvest(sourceToHarvest[0]);
-    } else {
-      //console.log("not on container")
-      creep.moveTo(containerBySource[0], {
-        visualizePathStyle: {
-          stroke: '#ffaa00'
-        }
       });
+      //console.log(allContainers)
+      var onContainer = false;
+      for (var i in allContainers) {
+        if (creep.pos.isEqualTo(allContainers[i])) {
+          onContainer = true;
+        }
+      }
+      //console.log("cont:" + container);
+      if (onContainer) {
+        //console.log("on container");
+        var sourceToHarvest = creep.pos.findInRange(FIND_SOURCES, 1);
+        creep.harvest(sourceToHarvest[0]);
+      } else {
+        //console.log("not on container")
+        creep.moveTo(containerBySource[0], {
+          visualizePathStyle: {
+            stroke: '#ffaa00'
+          }
+        });
+      }
     }
   }
 };
